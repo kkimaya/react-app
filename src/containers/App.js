ef9e8228db9ed1cb0a60bbe728b37bb1044f650d
Kamat <Kimaya.Kamat@msci.com>
2019-02-11 15:20:20 -0500

    Splitting App into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import './Person/Person.css';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-
-
+import Person from '../components/Persons/Person/Person';
+import Cockpit from '../components/Cockpit/Cockpit';
+import Persons from '../components/Persons/Persons';
 class App extends Component {
   state = {
     persons: [
@@ -15,18 +13,6 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   };
-
- /* switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };*/
 
     deletePersonHandler= (personIndex) => {
       const persons = this.state.persons.slice(); //make a copy of the array before splicing.
@@ -65,50 +51,22 @@ class App extends Component {
   render() {
 
     let persons=null;
-    let btnClass = '';
     if(this.state.showPersons){
-        persons=(
-             <div>
-                {this.state.persons.map((person,index) => {
-                    return <ErrorBoundary  key={person.id}>
-                                <Person
-                                click={() => this.deletePersonHandler(index)}
-                                name={person.name}
-                                age={person.age}
-                                changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                            </ErrorBoundary>
-                })}
-             </div>
-        );
-        btnClass = classes.Red;
-    }
-
-
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2)
-        assignedClasses.push(classes.red);
-    if(this.state.persons.length <= 1)
-            assignedClasses.push(classes.bold);
-
-    const rnd = Math.random();
-    if(rnd > 0.7){
-        throw new Error ("Something went wrong!")
+        persons=
+                <Persons
+                    persons={this.state.persons}
+                    clicked={this.deletePersonHandler}
+                    changed={this.nameChangedHandler}/>
     }
 
     return (
 
           <div className={classes.App}>
-            <h1>Hi, I'm a React App</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-
-            <button
-             className={btnClass}
-             onClick={this.togglePersonsHandler}>Switch Name</button>
-             { //ternary expression below: If showPersons is true show div, else (:) show null
-              //  this.state.showPersons === true ?
-               persons
-                 //: null
-             }
+             <Cockpit
+                showPersons={this.state.showPersons}
+                 persons={this.state.persons}
+                clicked={this.togglePersonsHandler}      />
+             {persons}
           </div>
 
     );
