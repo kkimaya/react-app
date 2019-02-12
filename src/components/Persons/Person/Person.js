309426a4082c103a31c217ece159197d37a95162
Kamat <Kimaya.Kamat@msci.com>
2019-02-12 16:38:07 -0500

    higher-order components WithClass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,7 +6,7 @@ class Person extends Component{
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Aux className={classes.Person} >
+            <Aux>
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old! </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed}  value={this.props.name} />
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
+import WithClass from '../hoc/WithClass';
 class App extends Component {
 
     constructor(props){
@@ -97,7 +98,7 @@ class App extends Component {
 
     return (
 
-          <div className={classes.App}>
+          <WithClass classes={classes.App}>
             <button onClick={() => {this.setState({showCockpit:false})}}>RemoveCockpit </button>
              {this.state.showCockpit?
              <Cockpit
@@ -108,7 +109,7 @@ class App extends Component {
 
               :null}
              {persons}
-          </div>
+          </WithClass>
 
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
