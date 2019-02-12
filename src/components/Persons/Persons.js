3355aa6f423436645e3e873478d10d203d085020
Kamat <Kimaya.Kamat@msci.com>
2019-02-12 15:27:41 -0500

    PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
     /*static getDerivedStateFromProps(props,state){
         console.log('[Persons.js] getDerivedStateFromProps',props);
@@ -13,10 +13,17 @@ class Persons extends Component {
          console.log('[Persons.js] componentWillReceiveProps', props);
     }*/
 
-    shouldComponentUpdate(nextProps,nextState){
-        console.log('[Persons.js] shouldComponentUpdate');
-        return true;
-    }
+    //shouldcomponentupdate should not be added in components which you're sure will update each time a parent component updates. that slows down your app processing.
+    //In our case, Persons doesn't update each time its parent component, cockpit, updates. So it is fine to use this method here. 
+    // shouldComponentUpdate(nextProps,nextState){
+    //     console.log('[Persons.js] shouldComponentUpdate');
+
+    //     if(nextProps.persons !== this.props.persons){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate');
